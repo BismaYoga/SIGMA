602291faaa54d457006c6ff79fe612a98690e4cf
Bisma Yoga <bismayoga432@gmail.com>
2025-07-05 14:13:27 +0800

    Menambahkan halaman landing page  dan CSS

---
+++ resources/views/landingpage.blade.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="initial-scale=1, width=device-width">
+  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+  <link rel="stylesheet" href=""/>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Gotham:wght@400;500;700&display=swap" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400&display=swap" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Gotham+Black:wght@400&display=swap" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Google+Sans+Display:wght@400;700&display=swap" />
+  <title>Beranda | SIGMA</title>
+</head>
+<body>
+
+  <div class="notloginhome">
+    
+    <!-- Header -->
+    <header class="header-notlogin">
+      <div class="header-notlogin"></div>
+      	<ul class="nav-menu">
+      		<li><a href="index.html" id="home">Home</a></li>
+      		<li><a href="peminjaan.html" id="peminjamanText">Peminjaman</a></li>
+      		<li><a href="informasi.html" id="informasiText">Informasi</a></li>
+      		<li><a href="bookingansaya.html" id="bookinganSayaText">Bookingan Saya</a></li>
+      		<li><a href="login.html" id="loginText">Login</a></li>
+		</ul>
+      <img class="logo-sigma-icon" alt="Logo SIGMA" src="/Assets/gambar/LOGO SIGMA 2 Vector Header.svg">
+    </header>
+
+    <main>
+      <!-- Hero Section sebagai Banner -->
+      <section class="hero" aria-labelledby="hero-heading">
+        <div class="hero-inner">
+            <div class="rectangle-container" id="rectangle-bookingsekarang">
+              <div class="rectangle-div"></div>
+              <div class="booking-sekarang ">Booking Sekarang</div>
+              
+              <div class="vector-parent">
+                <img class="vector-icon" alt="" src="/Assets/gambar/LOGO SIGMA 1 Vector.svg">
+
+                <header class="hero-text">
+                  <h1 id="hero-heading" class="sistem-informasi">Sistem Informasi</h1>
+                  <p class="peminjaman-gedung-mahasiswa">Peminjaman Gedung Mahasiswa</p>
+                </header>
+              </div>
+            </div>
+            <!-- <img class="hero-img" alt="" src="/Assets/gambar/gambar_head.png"> -->
+        </div>
+      </section>
+
+      <!-- Tentang SIGMA -->
+      <section aria-labelledby="about-sigmabox">
+        <div class="about-sigmaboxin">
+          <h2 id="about-sigma" class="about-sigma">
+          <span>About </span><span class="sigma">SIGMA</span>
+        </h2>
+        <p class="sigma-dikembangkan-untuk">
+          SIGMA dikembangkan untuk memberikan solusi digital dalam memfasilitasi proses peminjaman gedung dan perlengkapan di kampus. SIGMA diharapkan dapat mempercepat proses peminjaman dan memastikan transparansi dalam hal ketersediaan gedung, aturan peminjaman, dan informasi terkait gedung.
+        </p>
+
+        </div>
+        
+      </section>
+    </main>
+
+    <!-- Footer -->
+    <footer class="component-footer">
+      <div class="footer">
+        <div class="call-parent">
+          <section class="reservation">
+            <b class="reservasi">Reservasi</b>
+            <address class="call-reservasi">
+              <p>S: reservationfmipa@sigma.ac.id</p>
+              <p>T: +62 81234567891</p>
+            </address>
+          </section>
+          <section class="contact-center">
+            <b class="reservasi">Pusat Bantuan</b>
+            <address class="call-reservasi">
+              <p>S: contact@sigma.ac.id</p>
+              <p>T: +62 81234567891</p>
+            </address>
+          </section>
+        </div>
+
+        <div class="office-parent">
+          <section class="reservation">
+            <b class="reservasi">Kantor</b>
+            <address class="gedung-dekanat-fmipa-container" id="gedungDekanatFMIPA">
+              <p>Gedung Dekanat FMIPA lantai 4</p>
+              <p>Jl. Raya Kampus Unud, Bukit Jimbaran</p>
+            </address>
+          </section>
+          <section class="hours">
+            <b class="operasional">Operasional</b>
+            <div class="wita-monday-container">
+              <p>08.00 - 16.00 WITA</p>
+              <p>Monday - Friday</p>
+            </div>
+          </section>
+        </div>
+
+        <nav class="shortcut">
+          <div class="tentang-sigma" <a href="tentang-sigma.html" id="tentangSIGMAText">Tentang SIGMA  </a> </div>
+          <div class="faq" id="fAQText">FAQ</div>
+          <div class="sk" id="sKText">S&K</div>
+          <div class="kebijakan-privasi" id="kebijakanPrivasiText">Kebijakan Privasi</div>
+        </nav>
+
+        <div class="logo-sigma-footer">
+          <img class="group-logofooter" alt="Logo Group" src="/Assets/gambar/LOGO SIGMA 3 Vector Footer.svg">
+          <!-- <div class="group-div">
+            <div class="group-child1"></div>
+            
+          </div> -->
+          <!-- <img class="sigma-icon" alt="Logo SIGMA" src="SIGMA.svg">
+          <img class="udayana-icon" alt="Logo Udayana" src="UDAYANA.svg"> -->
+        </div>
+      </div>
+    </footer>
+
+  </div>
+
+  <script>
+    document.getElementById("rectangle-bookingsekarang")?.addEventListener("click", () => {});
+    document.getElementById("gedungDekanatFMIPA")?.addEventListener("click", () => {
+      window.open("https://maps.app.goo.gl/2kcbHk3bmaQo9Cqv7");
+    });
+
+    document.getElementById("tentangSIGMAText")?.addEventListener("click", () => { 'tentang-sigma.html' });
+    document.getElementById("fAQText")?.addEventListener("click", () => {});
+    document.getElementById("sKText")?.addEventListener("click", () => {});
+    document.getElementById("kebijakanPrivasiText")?.addEventListener("click", () => {});
+    document.getElementById("peminjamanText")?.addEventListener("click", () => {});
+    document.getElementById("informasiText")?.addEventListener("click", () => {});
+    document.getElementById("bookinganSayaText")?.addEventListener("click", () => {});
+    document.getElementById("loginText")?.addEventListener("click", () => {});
+  </script>
+</body>
+</html>
